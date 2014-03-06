61026dd8fce17440dcdf3a12e9f8912cc3ca0091
KM Tu <tutu522@gmail.com>
2014-03-07 04:44:28 +0900

    Driver sees the front car and can look over the next lane

--- lib/src/driver.dart
+++ lib/src/driver.dart
@@ -4,32 +4,53 @@ part of traffic_simulator;
 class Driver {
   final Vehicle vehicle;
   TrafficSimulator world;
-  
+  double safeDistance;
+  Lane nextLane;
   Driver(this.world, {this.vehicle});
   
   void update() {
-/*    DoubleLinkedQueueEntry<Vehicle> nextVehicle = vehicle.entry.nextEntry();
-    if (nextVehicle != null) {
-      double distance = nextVehicle.element.pos - nextVehicle.element.length - vehicle.pos;
-      if (distance < 5) {
-        vehicle.acc = -5.0;
+    if (nextLane == null) {
+      nextLane = vehicle.lane.laneEnd.last.joint.getRandomOutwardLane();
+    }
+    
+    DoubleLinkedQueueEntry<Vehicle> nextVehicleEntry = vehicle.entry.nextEntry();
+    Vehicle nextVehicle;
+    double distance;
+    if (nextVehicleEntry != null) {
+      nextVehicle = nextVehicleEntry.element;
+      distance = nextVehicle.pos - nextVehicle.length - vehicle.pos;
+    }
+    else {
+      if (nextLane.vehicle.isNotEmpty) {
+        nextVehicle = nextLane.vehicle.first;
+        distance = nextVehicle.pos - nextVehicle.length +
+            vehicle.lane.road.length - vehicle.pos;
       }
-      else if (distance > 10) {
-        vehicle.acc = 1.0;
+    }
+    
+    if (nextVehicle != null) {
+      safeDistance = vehicle.vel * vehicle.vel / (2 * vehicle.accMax);
+      if (distance < safeDistance && vehicle.vel > 0) {
+        vehicle.acc = -vehicle.accMax;
+      }
+      else if (distance > safeDistance) {
+        vehicle.acc = vehicle.accMax;
       }
       else 
       {
         vehicle.acc = 0.0;
       }
     }
-*/    
-    Road road = vehicle.lane.road;
-    if (vehicle.pos - vehicle.length > road.length) {
+    else {
+      vehicle.acc = vehicle.accMax;
+    }
+      
+    if (vehicle.pos - vehicle.length > vehicle.lane.road.length) {
       if (vehicle.lane.removeLastVehicle() != this.vehicle) {
         throw new StateError("The last removed vehicle must be the one who " 
                              "goes over the road end first.");
       }
-      vehicle.lane.laneEnd.last.joint.getRandomOutwardLane().addFirstVehicle(vehicle);
+      nextLane.addFirstVehicle(vehicle);
     }
   }
 }
