ec0acd5df06bc98f953f824a8e457e2d98fc5f21
KM Tu <tutu522@gmail.com>
2014-03-12 19:37:51 +0900

    Changes the key event management and the layout of creating world
    The keyboard control is not better! No annoying locking anymore.

--- web/trafficsim.dart
+++ web/trafficsim.dart
@@ -15,44 +15,56 @@ RunningState runningState = new RunningState();
 
 void main() {
   CanvasElement canvas = querySelector(".game-element");
-  canvas.width = 960;
-  canvas.height = 720;
+  canvas.width = 800;
+  canvas.height = 600;
   gameLoop = new GameLoopHtml(canvas);
+  gameLoop.processAllKeyboardEvents = false;
+  gameLoop.pointerLock.lockOnClick = false;
 
   world = new World(gameLoop);
 
   camera = new Camera(canvas, world, pixelPerMeter: 10.0);
 
-  List<Joint> joint = [new SourceJoint("0"), new Joint("1"),
-                       new Joint("2"), new Joint("3")];
+  List<Joint> joint = new List<Joint>(4);
+  joint[0] = new SourceJoint(label: "0", maxDispatch: 1);
+  joint[1] = new Joint(label: "1");
+  joint[2] = new SourceJoint(label: "2", maxDispatch: 2);
+  joint[3] = new SourceJoint(label: "3", maxDispatch: 3);
 
-  List<Vector2> p = [new Vector2(0.0, 0.0), new Vector2(500.0, 0.0),
-                     new Vector2(505.0, 0.0), new Vector2(1000.0, 0.0),
-                     new Vector2(1000.0, 5.0), new Vector2(505.0, -500.0),
-                     new Vector2(500.0, -500.0), new Vector2(0.0, -5.0),
-                     new Vector2(502.5, -495.0), new Vector2(502.5, 5.0),
-                     ];
+  List<Vector2> p = new List<Vector2>(10);
+  p[0] = new Vector2(0.0, 0.0);
+  p[1] = new Vector2(500.0, 0.0);
+  p[2] = new Vector2(505.0, 0.0);
+  p[3] = new Vector2(1000.0, 0.0);
+  p[4] = new Vector2(1000.0, 5.0);
+  p[5] = new Vector2(505.0, -500.0);
+  p[6] = new Vector2(500.0, -500.0);
+  p[7] = new Vector2(0.0, -5.0);
+  p[8] = new Vector2(502.5, -495.0);
+  p[9] = new Vector2(502.5, -5.0);
 
-  camera.setCenter(p[1]);
+  camera.center = p[1];
 
-  List<Road> road
-      = [new Road(p[0], p[1], numForwardLane: 3, numBackwardLane: 0),
-         new Road(p[2], p[3], numForwardLane: 3, numBackwardLane: 0),
-         new Road(p[4], p[5], numForwardLane: 3, numBackwardLane: 0),
-         new Road(p[6], p[7], numForwardLane: 3, numBackwardLane: 0),
-         new Road(p[8], p[9], numForwardLane: 3, numBackwardLane: 0)
-        ];
+  List<Road> road = new List<Road>(5);
+  road[0] = new Road(p[0], p[1], numForwardLane: 3, numBackwardLane: 0)
+          ..attachJoint(joint[0], Road.BEGIN_SIDE)
+          ..attachJoint(joint[1], Road.END_SIDE);
 
-  road[0].attachJoint(joint[0], Road.BEGIN_SIDE);
-  road[0].attachJoint(joint[1], Road.END_SIDE);
-  road[1].attachJoint(joint[1], Road.BEGIN_SIDE);
-  road[1].attachJoint(joint[2], Road.END_SIDE);
-  road[2].attachJoint(joint[2], Road.BEGIN_SIDE);
-  road[2].attachJoint(joint[3], Road.END_SIDE);
-  road[3].attachJoint(joint[3], Road.BEGIN_SIDE);
-  road[3].attachJoint(joint[0], Road.END_SIDE);
-  road[4].attachJoint(joint[3], Road.BEGIN_SIDE);
-  road[4].attachJoint(joint[1], Road.END_SIDE);
+  road[1] = new Road(p[2], p[3], numForwardLane: 3, numBackwardLane: 0)
+          ..attachJoint(joint[1], Road.BEGIN_SIDE)
+          ..attachJoint(joint[2], Road.END_SIDE);
+
+  road[2] = new Road(p[4], p[5], numForwardLane: 3, numBackwardLane: 0)
+          ..attachJoint(joint[2], Road.BEGIN_SIDE)
+          ..attachJoint(joint[3], Road.END_SIDE);
+
+  road[3] = new Road(p[6], p[7], numForwardLane: 3, numBackwardLane: 0)
+          ..attachJoint(joint[3], Road.BEGIN_SIDE)
+          ..attachJoint(joint[0], Road.END_SIDE);
+
+  road[4] = new Road(p[8], p[9], numForwardLane: 3, numBackwardLane: 0)
+          ..attachJoint(joint[3], Road.BEGIN_SIDE)
+          ..attachJoint(joint[1], Road.END_SIDE);
 
   world.addRoad(road);
   gameLoop.state = runningState;
@@ -74,35 +86,46 @@ class PauseState extends SimpleHtmlState {
   }
 
   void onKeyDown(KeyboardEvent event) {
-    event.preventDefault();
+    //    event.preventDefault();
     switch (event.keyCode) {
       case Keyboard.C:
+        event.preventDefault();
         camera.toCenter();
         break;
       case Keyboard.W:
+        event.preventDefault();
         camera.moveUp();
         break;
       case Keyboard.S:
+        event.preventDefault();
         camera.moveDown();
         break;
       case Keyboard.A:
+        event.preventDefault();
         camera.moveLeft();
         break;
       case Keyboard.D:
+        event.preventDefault();
         camera.moveRight();
         break;
       case Keyboard.Z:
+        event.preventDefault();
         camera.zoomIn(1.5);
         break;
       case Keyboard.X:
+        event.preventDefault();
         camera.zoomOut(1.5);
         break;
       case Keyboard.SPACE:
+        event.preventDefault();
         camera.stopMove();
         break;
-      default:
+      case Keyboard.TAB:
+        event.preventDefault();
         gameLoop.state = runningState;
         world.pause = false;
+        break;
+      default:
     }
   }
 }
@@ -120,12 +143,11 @@ class FPS {
     if (prevTime == null) {
       prevTime = new DateTime.now();
       lastShowPassedDuration = new Duration();
-    }
-    else {
+    } else {
       currentTime = new DateTime.now();
       Duration dt = currentTime.difference(prevTime);
       lastShowPassedDuration += dt;
-      fps = 0.05*fps + 0.95*(1000.0 / dt.inMilliseconds);
+      fps = 0.05 * fps + 0.95 * (1000.0 / dt.inMilliseconds);
       prevTime = currentTime;
     }
   }
@@ -151,35 +173,46 @@ class RunningState extends SimpleHtmlState {
   }
 
   void onKeyDown(KeyboardEvent event) {
-    event.preventDefault();
+    //    event.preventDefault();
     switch (event.keyCode) {
       case Keyboard.C:
+        event.preventDefault();
         camera.toCenter();
         break;
       case Keyboard.W:
+        event.preventDefault();
         camera.moveUp();
         break;
       case Keyboard.S:
+        event.preventDefault();
         camera.moveDown();
         break;
       case Keyboard.A:
+        event.preventDefault();
         camera.moveLeft();
         break;
       case Keyboard.D:
+        event.preventDefault();
         camera.moveRight();
         break;
       case Keyboard.Z:
+        event.preventDefault();
         camera.zoomIn(1.5);
         break;
       case Keyboard.X:
+        event.preventDefault();
         camera.zoomOut(1.5);
         break;
       case Keyboard.SPACE:
+        event.preventDefault();
         camera.stopMove();
         break;
-      default:
+      case Keyboard.TAB:
+        event.preventDefault();
         gameLoop.state = pauseState;
         world.pause = true;
+        break;
+      default:
     }
   }
 }
