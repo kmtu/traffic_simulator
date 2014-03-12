3e0e6a33eb0e34027b2b1d9fa592f6d6ffb6d990
KM Tu <tutu522@gmail.com>
2014-03-12 21:31:19 +0900

    Changes the white line pattern on the lane

--- lib/src/lane.dart
+++ lib/src/lane.dart
@@ -67,7 +67,7 @@ class Lane implements Backtraceable {
         // Outermost lane with another same-direction lane inside
         // Draw: inside white line
 //        _beginPathInsideLine(context);
-        _beginPathInsideDash(context, 2.0, 1.0);
+        _beginPathInsideDash(context, 5.0, 5.0);
         _strokeWhiteLine(context);
       }
     }
@@ -77,8 +77,8 @@ class Lane implements Backtraceable {
           // Outermost lane next to another same-directional lane.
           // This is a one-way traffic road with multiple lanes
 //        _beginPathInsideLine(context);
-          _beginPathInsideDash(context, 2.0, 1.0);
-          _beginPathOutsideDash(context, 2.0, 1.0);
+          _beginPathInsideDash(context, 5.0, 5.0);
+          _beginPathOutsideDash(context, 5.0, 5.0);
           _strokeWhiteLine(context);
         }
         else {
@@ -86,14 +86,14 @@ class Lane implements Backtraceable {
           // Draw: inside yello line, outside white line
           _beginPathInsideLine(context);
           _strokeSingleYellowLine(context);
-          _beginPathOutsideDash(context, 2.0, 1.0);
+          _beginPathOutsideDash(context, 5.0, 5.0);
           _strokeWhiteLine(context);
         }
       }
       else {
         // God bless it's just a simple middle lane!
-        _beginPathInsideDash(context, 2.0, 1.0);
-        _beginPathOutsideDash(context, 2.0, 1.0);
+        _beginPathInsideDash(context, 5.0, 5.0);
+        _beginPathOutsideDash(context, 5.0, 5.0);
         _strokeWhiteLine(context);
       }
     }
