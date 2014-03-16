c94ea74f304c5ef886db37312c4a0c074b169686
KM Tu <tutu522@gmail.com>
2014-03-17 01:26:55 +0900

    Trivial: changes just for git test

--- web/trafficsim.dart
+++ web/trafficsim.dart
@@ -13,8 +13,8 @@ void main() {
   int row = 3;
   int col = 5;
   double gap = 100.0;
-  int forward = 2;
-  int backward = 2;
+  int forward = 3;
+  int backward = 3;
   var roadGrid = createRoadGrid(row: row, col: col, gap: gap, forwardLane:
       forward, backwardLane: backward);
 
@@ -87,7 +87,7 @@ List<List<Road>> createRoadGrid({Vector2 p0, double gap: 50.0, int row: 5, int
   List<Joint> joint = new List<Joint>(p.length);
   for (var i = 0; i < joint.length; i++) {
     if (i % 3 == 1) {
-      joint[i] = new SourceJoint(label: "$i", maxSpawn: 1000);
+      joint[i] = new SourceJoint(label: "$i", maxSpawn: 100);
     } else {
       joint[i] = new Joint(label: "$i");
     }
